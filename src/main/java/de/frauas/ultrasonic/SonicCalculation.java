3a5f1e98dc49e76fc1c286157655a8330f4b391b
R4ijin <filiandkilifan249@gmail.com>
2025-05-15 11:59:25 +0200

    Updated the functions to fit to the needs of everyone

--- src/main/java/de/frauas/ultrasonic/SonicCalculation.java
+++ src/main/java/de/frauas/ultrasonic/SonicCalculation.java
@@ -1,5 +1,7 @@
 ﻿package de.frauas.ultrasonic;
 
+import de.frauas.scenario.components.Obstacle;
+import de.frauas.scenario.primitives.Vec2;
 import lombok.Getter;
 
 import java.util.HashSet;
@@ -8,58 +10,61 @@ import java.util.Set;
 public class SonicCalculation {
 
     @Getter
-    private static Set<SonicObject> objects = new HashSet<>();
+    private static Set<Obstacle> objects = new HashSet<>();
 
-    public static void addObject(SonicObject object) {
+
+    /**
+     * This method has to be used to add all obstacles to a list when loading the scenario
+     *
+     * @param object Obstacle
+     **/
+    public static void addObject(Obstacle object) {
         objects.add(object);
     }
 
+    /**
+     * Optional: Security feature to clear the list before inserting new obstacles from new scenario
+     **/
     public static void clearObjects() {
         objects.clear();
     }
 
-    public static SonicObject getNearestObject(SonicObject current, double headingDegrees) {
+    /**
+     * This method can be used to check whether there are any obstacles too close to the car and sends a boolen
+     * if the car needs to stop
+     *
+     * @param car Vec2 of the current location of the car
+     * @param headingDegrees Heading degree of the car
+     * @return Boolean whether the car needs to stop
+     */
+    public static boolean stop(Vec2 car, double headingDegrees) {
 
-        Set<SonicObject> visible = new HashSet<>();
-        for (SonicObject object : objects) {
-            if (isVisible(current, object, headingDegrees)) {
-                visible.add(object);
-            }
+        // Check all obstacles if they are too close
+        for (Obstacle obstacles : objects) {
+            double dx = obstacles.position.x() - car.x();
+            double dy = obstacles.position.y() - car.y();
+
+            double distance = Math.sqrt(dx * dx + dy * dy);
+
+            // If obstacle is too far away ignore the rest
+            if (distance > 3) return false;
+
+            // Analzye the heading direction of the heading degree
+            double headingRad = Math.toRadians(headingDegrees);
+            double viewX = Math.cos(headingRad);
+            double viewY = Math.sin(headingRad);
+
+            // Normalize
+            double targetVecX = dx / distance;
+            double targetVecY = dy / distance;
+
+            // Skalarproduct for determine the angle
+            double dot = viewX * targetVecX + viewY * targetVecY;
+            double angleRad = Math.acos(dot);
+            double angleDeg = Math.toDegrees(angleRad);
+
+            return angleDeg <= 15;
         }
-
-        SonicObject toReturn = null;
-        double min = Double.MAX_VALUE;
-        for (SonicObject object : visible) {
-            if (object.distanceTo(current) < min) {
-                min = object.distanceTo(current);
-                toReturn = object;
-            }
-        }
-        return toReturn;
-    }
-
-    public static boolean isVisible(SonicObject current, SonicObject target, double headingDegrees) {
-        double dx = target.x - current.x;
-        double dy = target.y - current.y;
-        double dz = target.z - current.z;
-
-        double distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
-        if (distance > 10) return false;
-
-        // Richtungsvektor aus Heading (2D-Sicht, Z wird ignoriert)
-        double headingRad = Math.toRadians(headingDegrees);
-        double viewX = Math.cos(headingRad);
-        double viewY = Math.sin(headingRad);
-
-        // Zielvektor normalisieren
-        double targetVecX = dx / distance;
-        double targetVecY = dy / distance;
-
-        // Skalarprodukt zur Winkelbestimmung
-        double dot = viewX * targetVecX + viewY * targetVecY;
-        double angleRad = Math.acos(dot);
-        double angleDeg = Math.toDegrees(angleRad);
-
-        return angleDeg <= 15;
+        return false;
     }
 }
