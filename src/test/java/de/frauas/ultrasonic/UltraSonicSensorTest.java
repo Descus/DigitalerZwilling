582bdf36e0b0da15125de4c6f8f008c5895137e5
Niklas Schmiedel <niklas.schmiedel@stud.fra-uas.de>
2025-05-16 15:00:28 +0200

    Change tests to utilize new Junit version

--- src/test/java/de/frauas/ultrasonic/UltraSonicSensorTest.java
+++ src/test/java/de/frauas/ultrasonic/UltraSonicSensorTest.java
@@ -3,6 +3,7 @@ package de.frauas.ultrasonic;
 import de.frauas.scenario.components.Obstacle;
 import de.frauas.scenario.primitives.SDF;
 import de.frauas.scenario.primitives.Vec2F;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -14,29 +15,33 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class UltraSonicSensorTest {
-    Vec2F position = new Vec2F(0, 50);
-    Vec2F forward = new Vec2F(1, 0);
+    Vec2F position;
+    Vec2F forward;
+    UltraSonicSensor sensor;
+    List<SDF> sdfs;
+    
+    @BeforeEach
+    public void init(){
+        position = new Vec2F(0, 50);
+        forward = new Vec2F(1, 0);
+        sensor = new UltraSonicSensor(new Vec2F(0, 0), forward);
+        sdfs = new ArrayList<>();
+    }
     
     @Test
     public void obstacleInRangeTest_ObstacleInFront_ShouldReturnTrue() {
-        UltraSonicSensor sensor = new UltraSonicSensor(new Vec2F(0, 0), forward);
-        List<SDF> sdfs = new ArrayList<>();
         sdfs.add(new Obstacle(new Vec2F(100, 0), new Vec2F(200, 100), 100));
         assertTrue(sensor.isObstacleInRange(position, sdfs));
     }
 
     @Test
     public void obstacleInRangeTest_ObstacleInRange_ShouldReturnTrue() {
-        UltraSonicSensor sensor = new UltraSonicSensor(new Vec2F(0, 0), forward);
-        List<SDF> sdfs = new ArrayList<>();
         sdfs.add(new Obstacle(new Vec2F(100, 60), new Vec2F(200, 100), 100));
         assertTrue(sensor.isObstacleInRange(position, sdfs));
     }
 
     @Test
     public void obstacleInRangeTest_ObstacleMissed_ShouldReturnFalse() {
-        UltraSonicSensor sensor = new UltraSonicSensor(new Vec2F(0, 0), forward);
-        List<SDF> sdfs = new ArrayList<>();
         sdfs.add(new Obstacle(new Vec2F(100, 200), new Vec2F(200, 300), 100));
         assertFalse(sensor.isObstacleInRange(position, sdfs));
     }
