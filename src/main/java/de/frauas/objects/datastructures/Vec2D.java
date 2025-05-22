d3b5eebe0d8b86085dbdcad16d2255707fc7426f
Niklas Schmiedel <niklas.schmiedel@stud.fra-uas.de>
2025-05-22 14:38:55 +0200

    Add Vec2 double Mock & Tests for implementation

---
+++ src/main/java/de/frauas/objects/datastructures/Vec2D.java
@@ -0,0 +1,53 @@
+package de.frauas.objects.datastructures;
+
+import lombok.Getter;
+
+@Getter
+public class Vec2D {
+    private double x, y;
+    
+    public Vec2D(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Vec2D add(Vec2D other) {
+        //TODO
+    }
+    
+    public Vec2D subtract(Vec2D other){
+        //TODO
+    }
+    
+    public Vec2D scale(double factor) {
+        //TODO
+    }
+    
+    public double dotProd(Vec2D other){
+        //TODO
+    }
+    
+    public double length() {
+        //TODO
+    }
+    
+    public double lengthSq(){
+        //TODO
+    }
+    
+    public Vec2D rotate(double angle) {
+        //TODO
+    }
+    
+    public Vec2D normalize() {
+        //TODO
+    }
+    
+    public Vec2D negate() {
+        //TODO
+    }
+    
+    public Vec2D perpendicuar(){
+        //TODO
+    }
+}
