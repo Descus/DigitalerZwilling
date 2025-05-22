9dfcfe1b45fdc080d918489a70a076c0035e5d41
Descus <idescusi@gmail.com>
2025-05-22 23:02:04 +0200

    Make stuff compilable again, no tests tho

--- src/main/java/de/frauas/objects/datastructures/Vec2D.java
+++ src/main/java/de/frauas/objects/datastructures/Vec2D.java
@@ -4,50 +4,60 @@ import lombok.Getter;
 
 @Getter
 public class Vec2D {
-    private double x, y;
+    private final double x, y;
     
     public Vec2D(double x, double y) {
         this.x = x;
         this.y = y;
     }
-    
+
     public Vec2D add(Vec2D other) {
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     public Vec2D subtract(Vec2D other){
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     public Vec2D scale(double factor) {
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     public double dotProd(Vec2D other){
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     public double length() {
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     public double lengthSq(){
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     public Vec2D rotate(double angle) {
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     public Vec2D normalize() {
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     public Vec2D negate() {
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     public Vec2D perpendicuar(){
         //TODO
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 }
