dcf2b72625bf45df511f5e5e0719349a4d5fabde
Sana <sanaalzeibak@gmail.com>
2025-05-23 18:04:16 +0200

    added Vector :)

--- src/main/java/de/frauas/objects/datastructures/Vec2D.java
+++ src/main/java/de/frauas/objects/datastructures/Vec2D.java
@@ -5,59 +5,60 @@ import lombok.Getter;
 @Getter
 public class Vec2D {
     private final double x, y;
-    
+
+
     public Vec2D(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
     public Vec2D add(Vec2D other) {
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new Vec2D(this.x + other.x , this.y + other.y);
     }
 
     public Vec2D subtract(Vec2D other){
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new Vec2D(this.x - other.x , this.y - other.y);
     }
 
     public Vec2D scale(double factor) {
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new Vec2D(this.x * factor , this.y * factor);
     }
 
     public double dotProd(Vec2D other){
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+        double result = this.x * other.x + this.y * other.y;
+        return result;
+
     }
 
     public double length() {
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+            double result = Math.sqrt(this.x * this.x + this.y * this.y);
+            return result;
     }
 
     public double lengthSq(){
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+        double result = this.x * this.x + this.y * this.y;
+        return result;
     }
 
     public Vec2D rotate(double angle) {
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+        double cosA = Math.cos(angle);
+        double sinA = Math.sin(angle);
+        double newX = this.x * cosA - this.y * sinA;
+        double newY = this.x * sinA + this.y * cosA;
+        Vec2D result = new Vec2D(newX, newY);
+        return result;
     }
 
     public Vec2D normalize() {
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+        double length = length();
+        return new Vec2D(this.x / length, this.y / length);
     }
 
     public Vec2D negate() {
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new Vec2D(this.x * -1, this.y * -1);
     }
     
     public Vec2D perpendicular(){
-        //TODO
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new Vec2D(this.y, this.x * -1);
     }
 }
