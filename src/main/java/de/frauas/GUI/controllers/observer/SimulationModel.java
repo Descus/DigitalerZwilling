075e36e93585951a3d91671bbbbe21dbe3b44ae3
The Pham <bp.programmieren@gmail.com>
2025-06-25 20:54:14 +0200

    Add Observer to centralize all the Timer in GUI

---
+++ src/main/java/de/frauas/GUI/controllers/observer/SimulationModel.java
@@ -0,0 +1,62 @@
+package de.frauas.GUI.controllers.observer;
+
+import de.frauas.objects.Scene;
+import de.frauas.objects.car.CarStatus;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimulationModel {
+    private final Timer timer;
+    private final List<SimulationObserver> observers = new ArrayList<>();
+    private boolean running = false;
+    private long lastTime = System.currentTimeMillis();
+
+    public SimulationModel(Scene scene) {
+        timer = new Timer(1000 / 60, e -> {
+            if (running) {
+                long now = System.currentTimeMillis();
+                double delta = (now - lastTime) / 1000.0;
+                lastTime = now;
+
+                if (scene.getCar().getStatus() == CarStatus.RUNNING) {
+                    scene.getCar().move(delta);
+                }
+                notifyObservers();
+            }
+        });
+    }
+
+    public void addObserver(SimulationObserver observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(SimulationObserver observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers() {
+        for (SimulationObserver o : observers) {
+            o.onSimulationUpdate();
+        }
+    }
+
+    public void start() {
+        running = true;
+        timer.start();
+    }
+
+    public void pause() {
+        running = false;
+    }
+
+    public void resume() {
+        running = true;
+    }
+
+    public void stop() {
+        running = false;
+        timer.stop();
+    }
+}
---
+++ src/main/java/de/frauas/GUI/controllers/observer/SimulationObserver.java
@@ -0,0 +1,5 @@
+package de.frauas.GUI.controllers.observer;
+
+public interface SimulationObserver {
+    void onSimulationUpdate();
+}
