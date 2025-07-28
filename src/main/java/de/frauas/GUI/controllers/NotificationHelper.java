9b92b7a685ec27cf99ed00b91de0f055214235b5
The Pham <bp.programmieren@gmail.com>
2025-07-28 16:01:11 +0200

    comment in code

--- src/main/java/de/frauas/GUI/controllers/NotificationHelper.java
+++ src/main/java/de/frauas/GUI/controllers/NotificationHelper.java
@@ -2,12 +2,22 @@ package de.frauas.GUI.controllers;
 
 import javax.swing.JOptionPane;
 
+/**
+ * NotificationHelper is a utility class to simplify showing popup dialogs
+ * for errors and warnings across the application.
+ */
 public class NotificationHelper {
 
     public static void showError( String message) {
         JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
     }
 
+    /**
+     * Displays a warning dialog with Yes/No options.
+     *
+     * @param message the warning message to be shown
+     * @return true if the user clicks "Yes", false otherwise
+     */
     public static boolean showWarning( String message) {
         int result = JOptionPane.showConfirmDialog(
                 null,
